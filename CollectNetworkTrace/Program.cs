7996c74ae9fc512e02c599b403cd9d962acaf4df
SYAMANTH NAGA VIVEK BEZAVADA <sybezava@microsoft.com>
2021-08-13 10:13:01 -0400

    Add project files.

---
+++ CollectNetworkTrace/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Configuration;
+
+namespace CollectNetworkTrace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int duration = Convert.ToInt32(ConfigurationManager.AppSettings["Duration"]);
+                var config = NetworkTraceConfiguation.GetConfiguration();
+                var authToken = AuthService.GetAcccessToken(config);
+                var armService = new ArmService(config, authToken);
+                Utility.Trace($"Chosen duration for Network Trace = {duration}");
+                armService.StartNetworkTrace(duration);
+            }
+            catch (Exception ex)
+            {
+                Utility.Trace($"Exception while running the tool {ex.GetType()}:{ex.Message} {Environment.NewLine} {ex.StackTrace}");
+            }
+            finally
+            {
+                Utility.FlushTrace();
+            }
+        }
+    }
+}
